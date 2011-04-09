16d73b896cbb52bda126037f8ee5f3a3b0b3e1bb
Faz <faz@faz-Aspire-5740.(none)>
2011-04-09 15:03:42 +0530

    First commit after changing prototype.js to jquery.js

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BhojanamPlain::Application.config.secret_token = 'ddae59d42ed0107c1e0d2df45366aead1fa26f59d50afea05b77ff7fc7321dd8501c959e71904f4d03f5dfa90f2e53413d3c77c05db43f45135b61ce7c67fbd6'
