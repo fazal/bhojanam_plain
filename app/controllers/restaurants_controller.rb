511842ba01af3eb86d9b753817ffc69dd29ae558
Faz <faz@faz-Aspire-5740.(none)>
2011-04-09 18:04:11 +0530

    Replace rails.js for jQuery

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -29,4 +29,11 @@ class RestaurantsController < ApplicationController
        render :action=>:edit
     end
   end
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = "Restaurant deleted successfully"
+    redirect_to restaurants_path
+  end
+  
 end
