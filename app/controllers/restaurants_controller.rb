7b35b6e24e09b7b9fdcb50072f72fd67e7f58e58
Faz <faz@faz-Aspire-5740.(none)>
2011-04-09 17:45:07 +0530

    Added edit functionality

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,7 +18,15 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
   def update
+    @restaurant = Restaurant.find(params[:id])
+    if(@restaurant.update_attributes(params[:restaurant]))
+      flash[:notice] = "Restaurant updated successfully"
+      redirect_to @restaurant
+    else
+       render :action=>:edit
+    end
   end
 end
